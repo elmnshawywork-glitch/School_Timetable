f710fd6c3c83fe6e1f742c04fc28144c6c3701b4
Ebda_edu.Ebda <elmnshawy.work@gmail.com>
2026-09-07 21:25:29 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.EBDA_CONFIG = {
+  "supa": {
+    "url": "https://gnqjnfmnqckirvtexknj.supabase.co",
+    "key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImducWpuZm1ucWNraXJ2dGV4a25qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3MTM1NTYsImV4cCI6MjEwNDI4OTU1Nn0.Rk_uOSsjsYeVe7-QPMLUXjdM3EuWtqm6ZWCcvIR3YCk",
+    "table": "ebda_state",
+    "row": "main"
+  },
+  "readSrc": "supa"
+};
